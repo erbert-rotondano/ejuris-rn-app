01fda04022e5b2c05c94f9f4b4b0b00e04c75e71
Erbert Rotondano <nimbus@Mac-mini-de-Nimbus.local>
2018-08-01 10:30:55 -0300

    adicionar função pra chamar o redux na edição de usuário. TODO: estruturar actions pra tratar parametros vazios

--- src/screens/UserProfile.js
+++ src/screens/UserProfile.js
@@ -8,6 +8,13 @@ import { connect } from 'react-redux';
 import Spinner from '../components/common/Spinner';
 
 class UserProfile extends Component {
+	state={
+		newUsername: '', 
+		newAddress: '', 
+		newPhone: '', 
+		newPassword: '', 
+		newPasswordConfirmation: ''
+	}
 	componentWillMount(){
 		this.loadUserInfo(); 
 	}
@@ -48,26 +55,141 @@ class UserProfile extends Component {
 	  		return(
 		  		<View>
 			  		<View style={styles.row}>
-						<Text style={styles.itemLabel}>Email: </Text><Text>{this.props.email || 'Informação não encontrada'}</Text>
+						<Text style={styles.itemLabel}>Email: </Text>
+							<Text>{this.props.email || 'Informação não encontrada'}</Text>
 					</View>
 			  		<View style={styles.row}>
-						<Text style={styles.itemLabel}>Nome: </Text><Text>{this.props.username || 'Informação não encontrada'}</Text>
+						<View style={styles.column}>
+							<Text style={styles.itemLabel}>Nome: </Text>
+							<TextInput
+					                style={styles.input}
+					                placeholder={this.props.username || 'Informação não encontrada'}
+					                autoCapitalize={'none'}
+					                returnKeyType={'next'}
+					                ref={(input) => this.username = input}
+					                autoCorrect={false}
+					                placeholderTextColor='#AAAAAA'
+					                underlineColorAndroid='transparent'
+					                onChangeText={(value) => this.setState({newUsername: value})}
+					                onSubmitEditing={() => this.phone.focus()} />
+						</View>
 					</View>
 					<View style={styles.row}>
-						<Text style={styles.itemLabel}>Telefone: </Text><Text>{this.props.phone || 'Informação não encontrada'}</Text>
+						<View style={styles.column}>
+							<Text style={styles.itemLabel}>Telefone: </Text>
+							<TextInput
+					                style={styles.input}
+					                placeholder={this.props.phone || 'Informação não encontrada'}
+					                autoCapitalize={'none'}
+					                returnKeyType={'next'}
+					                ref={(input) => this.phone = input}
+					                autoCorrect={false}
+					                placeholderTextColor='#AAAAAA'
+					                underlineColorAndroid='transparent'
+					                onChangeText={(value) => this.setState({newPhone: value})}
+					                onSubmitEditing={() => this.address.focus()} />
+					    </View>
 					</View>
 					<View style={styles.row}>
 						<Text style={styles.itemLabel}>Competência: </Text><Text>{this.props.competence || 'Informação não encontrada'}</Text>
 					</View>
 					<View style={styles.row}>
-						<Text style={styles.itemLabel}>Endereço: </Text><Text>{this.props.address || 'Informação não encontrada'}</Text>
+						<View style={styles.column}>
+							<Text style={styles.itemLabel}>Endereço: </Text>
+							<TextInput
+					                style={styles.input}
+					                placeholder={this.props.address || 'Informação não encontrada'}
+					                autoCapitalize={'none'}
+					                returnKeyType={'done'}
+					                ref={(input) => this.address = input}
+					                autoCorrect={false}
+					                placeholderTextColor='#AAAAAA'
+					                underlineColorAndroid='transparent'
+					                onChangeText={(value) => this.setState({newAddress: value})}/>
+						</View>
 					</View>
+					<View style={styles.row}>
+						<View style={styles.column}>
+							<Text style={styles.itemLabel}>Senha: </Text>
+							<TextInput
+					                style={styles.input}
+					                placeholder={'Digite aqui a nova senha'}
+					                autoCapitalize={'none'}
+					                returnKeyType={'next'}
+					                ref={(input) => this.password = input}
+					                autoCorrect={false}
+					                placeholderTextColor='#AAAAAA'
+					                underlineColorAndroid='transparent'
+					                onChangeText={(value) => this.setState({newPassword: value})}
+					                onSubmitEditing={() => this.password_confirmation.focus()} />
+						</View>
+					</View>
+					<View style={styles.row}>
+						<View style={styles.column}>
+							<Text style={styles.itemLabel}>Confirmação de Senha: </Text>
+							<TextInput
+					                style={styles.input}
+					                placeholder={'Confirme a senha'}
+					                autoCapitalize={'none'}
+					                returnKeyType={'done'}
+					                ref={(input) => this.password_confirmation = input}
+					                autoCorrect={false}
+					                placeholderTextColor='#AAAAAA'
+					                underlineColorAndroid='transparent'
+					                onChangeText={(value) => this.setState({newPasswordConfirmation: value})}/>
+		                </View>
+					</View>
+					<View style={styles.secondSection}>
+		        	<View style={styles.row}>
+		        		<View style={styles.column}>
+			        		<View style={{paddingTop: 15, paddingBottom: 15, flex: 1}}>
+				        		<Button
+				        		  style={{flex: 1}}
+								  raised
+								  backgroundColor={APP_COLOR}
+								  onPress={() => this.checkAndSendInfo()}
+								  title='Salvar Alterações' />	
+							  </View>
+		        		</View>
+		        	</View>
+	        	</View>
 				</View>
 	  		);
 	  	}
   	}
   	
   }
+  checkAndSendInfo(){
+  	const {newUsername, newAddress, newPhone, newPassword, newPasswordConfirmation } = this.state;
+  	console.log(newUsername, newAddress, newPhone, newPassword, newPasswordConfirmation );
+  	// let error = false;
+  	// let eName = false;
+  	// let eAddress = false;
+  	// let ePhone = false;
+  	// let ePassword = false;
+  	// if(newUsername != '' || newAddress != '' || newPhone != '' || newPassword != '' || newPasswordConfirmation != ''){
+  	// 	// Algo foi alterado...
+  		
+  	// 	if(newPassword != '' && newPasswordConfirmation != ''){
+  	// 		if(newPassword != newPasswordConfirmation){
+  	// 			ePassword = true;
+  	// 		}
+  	// 	}
+
+  	// 	if(newUsername == '' || newUsername == null){
+  	// 		eName = true;
+  	// 	}
+  	// 	if(newAddress == '' || newAddress == null){
+  	// 		eAddress = true;
+  	// 	}
+  	// 	if(newPhone == '' || newPhone == null){
+  	// 		ePhone = true;
+  	// 	}
+
+  		// chama o método com os parametros certos
+  		// if(eName !=)
+  	}
+  
 }
 
 const styles = {
@@ -110,6 +232,7 @@ const styles = {
 	},
 	column: {
 		flexDirection: 'column',
+		flex: 1
 	},
 	buttonsContainer: {
 		flexDirection: 'row',
